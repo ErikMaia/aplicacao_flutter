9e678c47876895f56a987f94e832066707494037
Erik Silva Maia <erikm384@gmail.com>
2023-04-13 09:14:44 -0300

    build: add sqflite and sheredPreference

--- aula5/lib/main.dart
+++ aula5/lib/main.dart
@@ -14,6 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pfe',
+      routes: {
+        '/': (context) => ProdutoCRUDPage(),
+        '/home': (context) => Text()
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
