6f7395aeb504a3329f33a98e86691047352c9636
Fabiano <fabiano-smi@hotmail.com>
2023-04-14 11:21:05 -0300

    #fabiano

--- aula5/lib/pages/cliente/cliente_list.dart
+++ aula5/lib/pages/cliente/cliente_list.dart
@@ -1,7 +1,7 @@
-import 'package:aula5/pages/cliente/cliente_edit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:aula5/models/cliente.dart';
 import '../../widgets/drawer_pages.dart';
+import 'cliente_edit.dart';
 
 class ClienteList extends StatefulWidget {
   const ClienteList({Key? key}) : super(key: key);
@@ -11,20 +11,21 @@ class ClienteList extends StatefulWidget {
 }
 
 class _ClienteListState extends State<ClienteList> {
-  final List<Map<String, String>> _clientes = [
-    {
-      'nome': 'Nome',
-      'sobrenome': 'sobrenome',
-      'endereco': 'endereco',
-      'telefone': 'telefone'
-    },
-    {
-      'nome': 'Nome',
-      'sobrenome': 'sobrenome',
-      'endereco': 'endereco',
-      'telefone': 'telefone'
-    },
-  ];
+  List<Cliente> _clientes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadClientes();
+  }
+
+  Future<void> _loadClientes() async {
+    final db = await Cliente.openDb();
+    final List<Cliente> clientes = await Cliente.readAll(db);
+    setState(() {
+      _clientes = clientes;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,24 +37,29 @@ class _ClienteListState extends State<ClienteList> {
       body: ListView.builder(
         itemCount: _clientes.length,
         itemBuilder: (BuildContext context, int index) {
+          final cliente = _clientes[index];
           return ListTile(
             title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(_clientes[index]['nome'] ?? ''),
+                Text(cliente.nome ?? ''),
                 const SizedBox(width: 5),
-                Text(_clientes[index]['sobrenome'] ?? ''),
+                Text(cliente.sobrenome ?? ''),
                 const SizedBox(width: 5),
-                Text(_clientes[index]['endereco'] ?? ''),
+                Text(cliente.endereco ?? ''),
                 const SizedBox(width: 5),
-                Text(_clientes[index]['telefone'] ?? ''),
+                Text(cliente.telefone ?? ''),
               ],
             ),
             onTap: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const ClienteEdit()),
-              );
+                MaterialPageRoute(builder: (context) => ClienteEdit()),
+              ).then((value) {
+                if (value != null && value) {
+                  _loadClientes();
+                }
+              });
             },
           );
         },
@@ -63,7 +69,11 @@ class _ClienteListState extends State<ClienteList> {
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => const ClienteEdit()),
-          );
+          ).then((value) {
+            if (value != null && value) {
+              _loadClientes();
+            }
+          });
         },
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add),
